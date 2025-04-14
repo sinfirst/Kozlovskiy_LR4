6e6c59928d5e5a8341219a4480a7e31bdb59629f
sinfirst <nikitakoz2006@gmail.com>
2025-04-14 20:11:01 +0300

    Правки по коду

--- main.cpp
+++ main.cpp
@@ -2,17 +2,54 @@
 #include <math.h>
 using namespace std;
 
-float readValues(){
-    float value;
-    cout<<"Введите число\n";
-    cin>>value;
+bool ValidateInputToInt(string input) {
+    if (input.empty()) return false;
+    try {
+        stoi(input);
+    } catch (...) {
+        return false;
+    }
+    return true;
+}
+
+
+bool ValidateInputToFloat(string input) {
+    if (input.empty()) return false;
+    try {
+        stof(input);
+    } catch (...) {
+        return false;
+    }
+    return true;
+}
+
+float EnterNumber(string label) {
+    string input;
+    cout << label << ": ";
+    getline(cin, input);
+    while (!ValidateInputToFloat(input)) {
+        cout << "Не валидный ввод!\n " << label << ": ";
+        getline(cin, input);
+    }
+    return stof(input);
+}
+
+float inputX(){
+    float value = EnterNumber("Введите число x");
+    return value;
+}
+float inputY(){
+    float value = EnterNumber("Введите число y");
     return value;
 }
 
+float inputZ(){
+    float value = EnterNumber("Введите число z");
+    return value;
+}
+
+
 int firstTask(float x, float y, float z){
-    cout<<x<<endl;
-    cout<<y<<endl;
-    cout<<z<<endl;
     return (int) x + y + z;
 }
 
@@ -21,9 +58,10 @@ float secondTask(float x, float y, float z){
 }
 
 int main(){
+    string input;
     int vibor;
     float x, y, z;
-    while(vibor != 0)
+    while(true)
     {
         cout<<"Выберите действие\n";
         cout<<"№1 Введение числа x\n";
@@ -31,23 +69,33 @@ int main(){
         cout<<"№3 Введение числа z\n";
         cout<<"№4 Целая часть суммы 3-х чисел\n";
         cout<<"№5 Дрбная часть среднего геометрического модулей 3-х чисел\n";
-        cout<<"Введите ноль чтобы завершить выполнение программы\n";
-        cin>>vibor;
+        cout<<"Введите 0 чтобы завершить выполнение программы\n";
+        getline(cin, input);
+        while (!ValidateInputToFloat(input)) {
+            cout << "Не валидный ввод!";
+            getline(cin, input);
+        }
+        vibor = stoi(input);
         switch(vibor){
         case 1:
-            x = readValues();
+            x = inputX();
             break;
         case 2:
-            y = readValues();
+            y = inputY();
             break;
         case 3:
-            z = readValues();
+            z = inputZ();
             break;
         case 4:
             cout<<firstTask(x, y, z)<<endl;
             break;
         case 5:
             cout<<secondTask(x, y, z)<<endl;
+            break;
+        case 0:
+            return 0;
+        default:
+            cout<<"Такого выбора нет!"<<endl;
             break;
         };
     }
